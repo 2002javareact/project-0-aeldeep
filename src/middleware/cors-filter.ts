ec50e9bf98c3b181d44e06122de12c031fe056c4
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-03-15 13:22:37 -0400

    Fixing CORS

--- src/middleware/cors-filter.ts
+++ src/middleware/cors-filter.ts
@@ -5,7 +5,8 @@ export function corsFilter(req:Request,res:Response,next){
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)//this is a hack, never do it in a real application
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
     res.header('Access-Control-Allow-Credentials', 'true')
-    if(req.method === 'OPTIONS'||req.method==='PATCH'){
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE')
+    if(req.method === 'OPTIONS'){
         //this is where we send the 'Pre Flight Response
         res.sendStatus(200)
     }else{
